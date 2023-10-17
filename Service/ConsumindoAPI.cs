4d3b32434f7816b07d9db302fd971aa60c91ff84
Felipe Mendes Alexandrino <felipe.alexandrino@domlum.local>
2023-10-17 12:32:24 -0300

    5 - Removendo o metodo de consulta NrRegistro1

--- Service/ConsumindoAPI.cs
+++ Service/ConsumindoAPI.cs
@@ -4,23 +4,6 @@ namespace RoboRegisAPI.Service;
 public class ConsumindoAPI
 {
     public DesserializarJson _desser;
-    
-    public async Task ConsultaNmRegistro(string Nregistro){
-
-        using (HttpClient client = new HttpClient())
-        {          
-            client.DefaultRequestHeaders.Add("Authorization" ,"Guest");            
-            
-            string url = $"https://consultas.anvisa.gov.br/api/consulta/saude?count=10&filter%5BnumeroRegistro%5D={Nregistro}&page=1";
-            var response = await client.GetAsync(url);
-
-            var content = await response.Content.ReadAsStringAsync();
-            
-            Console.WriteLine(content);
-        }
-	
-    }
-
     public async Task ConsultaNmRegistro2(string Nregistro){
 
         using (HttpClient client = new HttpClient())
