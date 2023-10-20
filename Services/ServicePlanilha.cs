fa8069aaf9f74b512c3158e4fc66c37bd027437a
Felipe Mendes Alexandrino <felipe.alexandrino@domlum.local>
2023-10-20 17:15:15 -0300

    -----FEATURE BETA-----

--- Services/ServicePlanilha.cs
+++ Services/ServicePlanilha.cs
@@ -93,15 +93,12 @@ public class ServicePlanilha
                 var itens = item[i]; 
                 worksheet.Cells[i + 2, 1].Value = Convert.ToInt32(i+1);
                 worksheet.Cells[i + 2, 2].Value = itens.nomeProduto;
-                worksheet.Cells[i + 2, 3].Value = Convert.ToInt64(itens.processo);
-                worksheet.Cells[i + 2, 4].Value = Convert.ToInt64(itens.registro);
+                worksheet.Cells[i + 2, 3].Value = itens.processo;
+                worksheet.Cells[i + 2, 4].Value = itens.registro;
                 worksheet.Cells[i + 2, 5].Value = itens.razaoSocial;
                 worksheet.Cells[i + 2, 6].Value = itens.cnpj;
-                worksheet.Cells[i + 2, 7].Value = Convert.ToInt32(itens.situacao);
-                worksheet.Cells[i + 2, 8].Value = Convert.ToDateTime(itens.dataVencimento).ToString("d");
-                worksheet.Cells[i + 2, 9].Value = Convert.ToInt32(itens.codigoTipo);
-                worksheet.Cells[i + 2, 10].Value = itens.descSituacao;
-                worksheet.Cells[i + 2, 11].Value = itens.descTipo;
+                worksheet.Cells[i + 2, 7].Value = Convert.ToDateTime(itens.dataVencimento).ToString("d");
+                worksheet.Cells[i + 2, 8].Value = itens.descSituacao;
             }
 
         }
@@ -121,11 +118,8 @@ public class ServicePlanilha
             worksheet.Cells[1, 4].Value = "Registro";
             worksheet.Cells[1, 5].Value = "Razão Social";
             worksheet.Cells[1, 6].Value = "CNPJ";
-            worksheet.Cells[1, 7].Value = "Situação";
-            worksheet.Cells[1, 8].Value = "Data de Vencimento";
-            worksheet.Cells[1, 9].Value = "Código do Tipo";
-            worksheet.Cells[1, 10].Value = "Descrição da Situação";
-            worksheet.Cells[1, 11].Value = "Descrição do Tipo";
+            worksheet.Cells[1, 7].Value = "Data de Vencimento";
+            worksheet.Cells[1, 8].Value = "Descrição da Situação";
 
         }
         catch(Exception e)
