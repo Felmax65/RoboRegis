2a2df665d40f96d453f674c25e1c6c92d55dafc2
Felipe Mendes Alexandrino <felipe.alexandrino@domlum.local>
2023-11-06 10:53:59 -0300

    RRegis 1.3 - Atualizado

--- Service/ServicePlanilha.cs
+++ Service/ServicePlanilha.cs
@@ -55,70 +55,89 @@ public class ServicePlanilha{
             var data1 = DateTime.Now;
             var caminho = @$"C:\RoboRegis\\Dados-RoboRegis\Saida\Registros-{data.Replace("/","")}.xlsx"; //Caminho para salvar a planilha
             if (caminho != null && vigentes != null){ //Verificação de nulo
-                using (var package = new ExcelPackage(new FileInfo(caminho))){
+                using (var package = new ExcelPackage(new FileInfo(caminho)))
+                {
 
                     var vigentesSheet = package.Workbook.Worksheets.Add("Registros_Vigentes");
 
-                    // Adiciona as colunas iniciais
-                    vigentesSheet.Cells[1, 1].Value = "PRODUTO";
-                    vigentesSheet.Cells[1, 2].Value = "REGISTRO";
-                    vigentesSheet.Cells[1, 3].Value = "PROCESSO";
-                    // Adiciona as colunas adicionais                    
-                    vigentesSheet.Cells[1, 4].Value = "CNPJ";
-                    vigentesSheet.Cells[1, 5].Value = "RAZÃO SOCIAL";
-                    vigentesSheet.Cells[1, 6].Value = "STATUS";
-                    vigentesSheet.Cells[1, 7].Value = "DATA";
+                    PreencherCabecalho(vigentesSheet); // Cabelçalho da planilha
 
                     int row = 2; // Começa a partir da segunda linha
 
-                    foreach (var root in vigentes){
-                        // Preenche as colunas iniciais
-                        vigentesSheet.Cells[row, 1].Value = root.produto;
-                        vigentesSheet.Cells[row, 2].Value = root.registro;
-                        vigentesSheet.Cells[row, 3].Value = root.processo;
-
-                        if (root.empresa != null){
-                            vigentesSheet.Cells[row, 4].Value = root.empresa.cnpj;
-                            vigentesSheet.Cells[row, 5].Value = root.empresa.razaoSocial;
-                        }
-                        
-                        if(root.cancelado == "true"){
-                            vigentesSheet.Cells[row, 6].Value = root.cancelado = "CANCELADO";
-                            vigentesSheet.Cells[row, 7].Value = Convert.ToDateTime(root.dataCancelamento).ToString("d");
-                        }                       
-                        
-                        if (root.vencimento != null){                            
-                            vigentesSheet.Cells[row, 6].Value = root.vencimento.descricao;
-                            
-                            if(root.vencimento.data != null){
-
-                                var dtCompara = DateTime.Compare(Convert.ToDateTime(root.vencimento.data),data1);
-
-                                if(dtCompara > 0){
-                                    vigentesSheet.Cells[row, 6].Value = "VENCE EM";
-                                    vigentesSheet.Cells[row, 7].Value = Convert.ToDateTime(root.vencimento.data).ToString("d");
-                                }
-                                else{
-                                    vigentesSheet.Cells[row, 6].Value = "VENCIDO";
-                                    vigentesSheet.Cells[row, 7].Value = Convert.ToDateTime(root.vencimento.data).ToString("d");
-                                }
-                            }
-                        }
-
-
-                        row++; // Avança para a próxima linha
-                    }
+                    row = PreencherColunas(vigentes, data1, vigentesSheet, row); //Metodo para preencher as colunas
 
                     package.Save(); //Salva a planilha
-                    Console.ForegroundColor = ConsoleColor.Green;   
-                    Console.WriteLine("-Planilha gerada com sucesso em C:\\RoboRegis\\Dados-RoboRegis\\Saida\\");         
-                }   
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("-Planilha gerada com sucesso em C:\\RoboRegis\\Dados-RoboRegis\\Saida\\");
+                }
             }
         }
         catch(Exception e){
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"-Erro ao gerar a planilha: {e.Message}");
         }
+    }
+
+    private static void PreencherCabecalho(ExcelWorksheet vigentesSheet)
+    {
+        // Adiciona as colunas iniciais
+        vigentesSheet.Cells[1, 1].Value = "PRODUTO"; // Coluna 1
+        vigentesSheet.Cells[1, 2].Value = "REGISTRO"; // Coluna 2
+        vigentesSheet.Cells[1, 3].Value = "PROCESSO"; // Coluna 3
+        // Adiciona as colunas adicionais                    
+        vigentesSheet.Cells[1, 4].Value = "CNPJ"; //Coluna 4
+        vigentesSheet.Cells[1, 5].Value = "RAZÃO SOCIAL";//Coluna 5
+        vigentesSheet.Cells[1, 6].Value = "STATUS";//Coluna 6
+        vigentesSheet.Cells[1, 7].Value = "DATA";//Coluna 7
+    }
+
+    private static int PreencherColunas(List<RootVigente> vigentes, DateTime data1, ExcelWorksheet vigentesSheet, int row)
+    {
+        foreach (var root in vigentes)
+        {
+            // Preenche as colunas iniciais
+            vigentesSheet.Cells[row, 1].Value = root.produto; // Insere o nome do Produto Coluna 1
+            vigentesSheet.Cells[row, 2].Value = root.registro; // insere o numero do Registro Coluna 2
+            vigentesSheet.Cells[row, 3].Value = root.processo; // Insere o numero do Processo Coluna 3
+
+            if (root.empresa != null) // Verifica se nome da empresa é diferente de nulo
+            {
+                vigentesSheet.Cells[row, 4].Value = root.empresa.cnpj; // Insere o nome da emoresa na coluna 4
+                vigentesSheet.Cells[row, 5].Value = root.empresa.razaoSocial; // Insere a razão socual da empresa na coluna 5
+            }
+
+            if (root.cancelado == "true") // Verifica se a variavel cancelado é igual a true
+            {
+                vigentesSheet.Cells[row, 6].Value = root.cancelado = "CANCELADO"; // Insere o status "CANCELADO" na coluna STATUS coluna 6
+                vigentesSheet.Cells[row, 7].Value = Convert.ToDateTime(root.dataCancelamento).ToString("d"); // Insere a data de cancelamento na coluna 7
+            }
+
+            if (root.vencimento != null) // Verifica se vencimento é diferente de nulo
+            {
+                vigentesSheet.Cells[row, 6].Value = root.vencimento.descricao; // Insere o Status VIGENTE na coluna STATUS 
+
+                if (root.vencimento.data != null) // Verifica se data é diferente de nulo
+                {
+
+                    var dtCompara = DateTime.Compare(Convert.ToDateTime(root.vencimento.data), data1); // Compara se a data Vencimento é maior do que a data Atual
+
+                    if (dtCompara > 0) // Se data vencimento maior que 0
+                    {
+                        vigentesSheet.Cells[row, 6].Value = "VENCE EM"; // Insere  VENCE EM na coluna STATUS 
+                        vigentesSheet.Cells[row, 7].Value = Convert.ToDateTime(root.vencimento.data).ToString("d"); // Insere a data na coluna 7
+                    }
+                    else
+                    {
+                        vigentesSheet.Cells[row, 6].Value = "VENCIDO";// Insere  VENCIDO na coluna STATUS 
+                        vigentesSheet.Cells[row, 7].Value = Convert.ToDateTime(root.vencimento.data).ToString("d");// Insere a data na coluna 7
+                    }
+                }
+            }
+
+            row++; // Avança para a próxima linha
+        }
+
+        return row;
     }
     #endregion
 }
